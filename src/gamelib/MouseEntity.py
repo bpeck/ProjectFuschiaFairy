eed34e3743582029e85779bae1ba0df9801a4654
Ben Peck <bvpeck@gmail.com>
2012-01-28 18:26:34 -0800

    optimizing the mouse entity a bit

--- src/gamelib/MouseEntity.py
+++ src/gamelib/MouseEntity.py
@@ -18,27 +18,14 @@ class MouseEntity(Entity, InputListener):
     def __init__(self):
         Entity.__init__(self)
         self.name = 'MouseEntity'
-
-        #self.rect = Rect(0,0,25,25)
-        self.pos = [0.0,0.0]
-       # self.pos[0] = -self.rect.center[0]
-       # self.pos[1] = -self.rect.center[1]
-        self.mouse_pos = Vect2((0, 0))
+        self.mouse_pos = Vect2([0.0, 0.0])
         self.image = pygame.image.load('data/mousePlayer.png')
-       # self.image.set_colorkey((0,0,0))
-       # self.image.lock()
-      #  draw.circle(self.image, (255,255,255), self.rect.center, self.rect.w/2, 1)
-      #  self.image.unlock()
+        self.mouseOffset = Vect2(self.image.get_rect().center)
     
     def processEvent(self, event, dT=0):
         if event.type == pygame.MOUSEMOTION:
             # center the position at the mouse
-            self.pos[0] = event.pos[0] - self.image.get_rect().center[0]
-            self.pos[1] = event.pos[1] - self.image.get_rect().center[1]
-            self.mouse_pos = Vect2(event.pos)
-    
-    
-
+            self.mouse_pos = Vect2(event.pos) - self.mouseOffset
     
     def update(self, dT=0):
         self.pos = self.mouse_pos
