7e564bb195d2e84f2a0fff0eae2d6bbb8ee80c70
Colin Socha <csocha@gmail.com>
2012-01-29 17:27:12 -0500

    add all

--- src/gamelib/Game.py
+++ src/gamelib/Game.py
@@ -1,5 +1,5 @@
 # py imports
-import os, sys
+import os, sys, time, random, math
 
 # pygame imports
 import pygame
@@ -12,7 +12,8 @@ from Arena import Arena
 from TestArena import TestArena
 from Entity import Entity
 from InputListener import InputListener
-
+from Prey import Prey
+from Predator import Predator
 #from Collision import Collision
 
 import Variables
@@ -25,7 +26,8 @@ class Game(object):
         self.screen = screen
         self.currentLevel = 0
         self.done = False
-        
+        self.deadPredator = []
+        self.deadPrey = []
         self.currentArea = TestArena(screen ,self.currentLevel)
         
         self.entities = self.currentArea.getInitialEntities()
@@ -79,9 +81,35 @@ class Game(object):
                     entity.render(self.screen)
 
                 for e in entitiesToRemove:
+                    if isinstance(e, Predator):
+                        self.deadPredator.append(time.time())
+                    if isinstance(e, Prey):
+                        self.deadPrey.append(time.time())
                     self.entities.remove(e)
-
-
+                    #print "removed a guy!"
+                maxSize = 50.0
+                minSize = 6.0
+                # rotation accel rates
+                maxRotVel = math.pi / 20.0
+                maxRotAcc = math.pi / 40.0
+                for i in self.deadPrey:
+                    if time.time() > i +3:
+                        size = minSize + random.random() * (maxSize - minSize)
+                        pos = [0.0,0.0]
+                        pos[0] += int(random.random() * screenW)
+                        pos[1] += int(random.random() * screenH)            
+                        prey = Prey(size, pos, maxRotVel, maxRotAcc, self.currentArea)
+                        self.entities.append(prey)
+                        self.deadPrey.remove(i)
+                for i in self.deadPredator:
+                    if time.time() > i +3:
+                        size = minSize + random.random() * (maxSize - minSize)
+                        pos = [0.0,0.0]
+                        pos[0] += int(random.random() * 640)
+                        pos[1] += int(random.random() * 480)            
+                        predator = Predator(size, pos, maxRotVel, maxRotAcc, self.currentArea)
+                        self.entities.append(predator)
+                        self.deadPredator.remove(i)
                 # exert attract/repulse forces - this is shitty
                 #self.currentArea.doAccelerators()
 
