f954c60951bcb5219d30c402ac64a611da0b0ce7
unknown <Jim@Orko.(none)>
2012-01-28 16:47:02 -0800

    redesign of actor / circleguy / entity

--- src/gamelib/CircleGuy.py
+++ src/gamelib/CircleGuy.py
@@ -3,26 +3,52 @@ from pygame.rect import Rect
 from pygame import Surface
 from pygame import draw
 
-from Actor import Actor
+from Entity import Entity
 from InputListener import InputListener
 from Vect2 import Vect2
 
-class CircleGuy(Actor, InputListener):
+class CircleGuy(Entity, InputListener):
     
     def __init__(self):
-        Actor.__init__(self, 0.5)
+        Entity.__init__(self)
         self.name = 'CircleGuy'
         self.pos = Vect2((100.0, 100.0))
         self.rect = Rect(0,0,50,50)
+        
+        self.keys = [False]*512
         
         self.image = Surface(self.rect[2:])
         self.image.set_colorkey((0,0,0))
         self.image.lock()
         draw.circle(self.image, (255,0,0), self.rect.center, self.rect.w/2, 1)
         self.image.unlock()
+        
+        self.speed = 0.5
+        self.drag  = 0.9
     
     def processEvent(self, event, dT=0):
         if event.type == pygame.KEYDOWN:
             self.keyDown(event.key)
         if event.type == pygame.KEYUP:
             self.keyUp(event.key)
+
+    def keyDown(self, k):
+        self.keys[k] = True
+        
+    def keyUp(self, k):
+        self.keys[k] = False
+    
+    def update(self,DT):
+        self.acc = Vect2([0, 0])
+        if self.keys[pygame.K_w]: self.acc[1] -= self.speed
+        if self.keys[pygame.K_a]: self.acc[0] -= self.speed
+        if self.keys[pygame.K_s]: self.acc[1] += self.speed
+        if self.keys[pygame.K_d]: self.acc[0] += self.speed
+
+        self.move()
+        
+		# wrap around the playfield
+        if self.pos[0] > 640: self.pos[0] = 0
+        if self.pos[0] < 0:   self.pos[0] = 640
+        if self.pos[1] > 480: self.pos[1] = 0
+        if self.pos[1] < 0:   self.pos[1] = 480
--- src/gamelib/Entity.py
+++ src/gamelib/Entity.py
@@ -1,13 +1,26 @@
 from pygame import Surface
 from pygame.sprite import Sprite
 
+from Vect2 import Vect2
+
 class Entity(Sprite):
 
     def __init__(self):
         self.rect = None
         self.image = None
         
-        self.pos = [0.0, 0.0]
+        self.pos = Vect2([0.0, 0.0])
+        self.vel = Vect2([0.0, 0.0])
+        self.acc = Vect2([0.0, 0.0])
+        self.maxSpeed = -1
+        self.drag = 1
+        
+    def move(self):
+        self.vel += self.acc
+        if self.maxSpeed != -1 and self.vel.magnitude() > self.maxSpeed:
+          self.vel = self.vel.normalize(self.maxSpeed)
+        self.vel *= self.drag
+        self.pos += self.vel
     
     def render(self, screen, dT=0.0):
         screen.blit(self.image, (int(self.pos[0]), int(self.pos[1])))
